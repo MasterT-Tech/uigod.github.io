ca3284274bf9c37642fa4e4c0294ab2bd29227d4
Timothy Awinia <65822208+NewAgeDesign@users.noreply.github.com>
2023-08-03 15:27:27 +0300

    Add files via upload
    Signed-off-by: Timothy Awinia <65822208+NewAgeDesign@users.noreply.github.com>

--- app.js
+++ app.js
@@ -1,23 +1,21 @@
 io.root({ 
   light: {
-    '--grad': 'linear-gradient(180deg,#2a6ada,#0b42a1)',
-    '--disabledtxt' : 'rgba(0, 0, 0, .2)',
-    '--tertiary': 'rgb(241, 183, 22)',
-    '--quartenary': '#141420',
-    '--secondary': '#ffffff',
-    '--primary': '#d4d6e9',
-    '--accent2': 'rgb(241, 183, 22)',
-    '--accent': '#0e1d36'
+    '--primary': '#dbdce7',
+    '--secondary': '#aeadc5',
+    '--tertiary': '#6b6b8b',
+    '--quartenary': '#100f1f',
+    '--accent': '#53547c',
+    '--complementary': '#030111',
+    '--grad': 'linear-gradient(180deg,var(--primary),var(--quartenary))'
   },
   dark: {
-    '--grad': 'linear-gradient(180deg,#dfe20d,#e27f0d)',
-    '--disabledtxt' : '#0e1d36',
-    '--tertiary': 'rgb(241, 183, 22)',
-    '--accent': 'rgb(241, 183, 22)',
-    '--quartenary': '#ffffff',
-    '--secondary': '#0e1d36',
-    '--accent2': '#0e1d36',
-    '--primary': '#051124',
+    '--primary': '#06021d',
+    '--secondary': '#121135',
+    '--tertiary': '#5249a3',
+    '--quartenary': '#c7c1e7',
+    '--accent': '#444379',
+    '--complementary': '#cecbf1',
+    '--grad': 'linear-gradient(180deg,var(--primary),var(--quartenary))'
   }
 });
 
